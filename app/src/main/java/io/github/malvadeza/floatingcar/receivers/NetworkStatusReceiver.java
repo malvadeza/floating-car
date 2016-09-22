a75438bb2923f67d7a208f029ca4f5d3babc958b
Antonio Ribeiro <tonhogeek@gmail.com>
2016-09-22 18:03:08 -0300

    Little improvement on SyncService and update graddle dependencies

--- app/src/main/java/io/github/malvadeza/floatingcar/receivers/NetworkStatusReceiver.java
+++ app/src/main/java/io/github/malvadeza/floatingcar/receivers/NetworkStatusReceiver.java
@@ -9,10 +9,6 @@ import android.util.Log;
 
 import io.github.malvadeza.floatingcar.services.SyncService;
 
-
-/**
- * Created by tonho on 13/09/16.
- */
 public class NetworkStatusReceiver extends BroadcastReceiver {
     private static final String TAG = NetworkStatusReceiver.class.getSimpleName();
 
@@ -27,7 +23,11 @@ public class NetworkStatusReceiver extends BroadcastReceiver {
             /* Start service for upload data */
 
             Intent serviceIntent = new Intent(context, SyncService.class);
-            serviceIntent.setAction(SyncService.SYNC_TRIP);
+            serviceIntent.setAction(SyncService.SYNC_TRIPS);
+            context.startService(serviceIntent);
+
+            serviceIntent = new Intent(context, SyncService.class);
+            serviceIntent.setAction(SyncService.SYNC_SAMPLES);
             context.startService(serviceIntent);
         }
     }
--- app/src/main/java/io/github/malvadeza/floatingcar/services/SyncService.java
+++ app/src/main/java/io/github/malvadeza/floatingcar/services/SyncService.java
@@ -10,30 +10,26 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.URL;
-
 import io.github.malvadeza.floatingcar.LoggingService;
 import io.github.malvadeza.floatingcar.data.FloatingCarContract;
 import io.github.malvadeza.floatingcar.data.FloatingCarDbHelper;
 
 public class SyncService extends IntentService {
     private static final String TAG = SyncService.class.getSimpleName();
-    public static final String SYNC_TRIP =
+    public static final String SYNC_TRIPS =
             "io.github.malvadeza.floatingcar.syncservice.sync_trip";
+    public static final String SYNC_SAMPLES =
+            "io.github.malvadeza.floatingcar.syncservice.sync_sample";
 
-    private final Uri BASE_URI;
+    private static final Uri BASE_URI = new Uri.Builder()
+            .scheme("http")
+            .authority("")
+            .build();;
 
     public SyncService() {
         super("SyncService");
 
         Log.d(TAG, "SyncService");
-
-        BASE_URI = new Uri.Builder()
-                .scheme("http")
-                .authority("")
-                .build();
-
-
     }
 
     @Override
@@ -45,33 +41,39 @@ public class SyncService extends IntentService {
         }
 
         switch (intent.getAction()) {
-            case SYNC_TRIP: {
-                syncTrip();
-
+            case SYNC_TRIPS: {
+                syncTrips();
+                break;
+            }
+            case SYNC_SAMPLES: {
+                syncSamples();
                 break;
             }
         }
     }
 
-    private void syncTrip() {
-        Log.d(TAG, "syncTrip");
+    private void syncTrips() {
+        Log.d(TAG, "syncTrips");
 
-        FloatingCarDbHelper dbHelper = new FloatingCarDbHelper(this);
+        FloatingCarDbHelper dbHelper = FloatingCarDbHelper.getInstance(getApplicationContext());
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         Cursor trips = db.query(FloatingCarContract.TripEntry.TABLE_NAME,
                 new String[]{
                         FloatingCarContract.TripEntry.STARTED_AT,
                         FloatingCarContract.TripEntry.FINISHED_AT,
+                        FloatingCarContract.TripEntry.SYNCED,
                         FloatingCarContract.TripEntry.SHA_256
                 },
-                FloatingCarContract.TripEntry.SYNCED + " = ? ",
+                FloatingCarContract.TripEntry.SYNCED + " = ?"
+                        + " AND " + FloatingCarContract.TripEntry.FINISHED_AT + " IS NOT NULL",
                 new String[]{"0"}, null, null, null
         );
 
         try {
             final int startedAtIndex = trips.getColumnIndex(FloatingCarContract.TripEntry.STARTED_AT);
             final int finishedAtIndex = trips.getColumnIndex(FloatingCarContract.TripEntry.FINISHED_AT);
+            final int syncedIndex = trips.getColumnIndex(FloatingCarContract.TripEntry.SYNCED);
             final int shaIndex = trips.getColumnIndex(FloatingCarContract.TripEntry.SHA_256);
 
             while (trips.moveToNext()) {
@@ -81,11 +83,61 @@ public class SyncService extends IntentService {
                 trip.put(FloatingCarContract.TripEntry.SHA_256, trips.getString(shaIndex));
 
                 sendJSONObject(trip, "trip");
+
+                /* if syncing with server is successful then update SYNCED column */
+//                ContentValues tripValue = new ContentValues();
+//                trip.put(FloatingCarContract.TripEntry.SYNCED, "1");
+//
+//                db.update(FloatingCarContract.TripEntry.TABLE_NAME, tripValue,
+//                        FloatingCarContract.TripEntry.SHA_256 + " = ?",
+//                        new String[]{trips.getString(shaIndex)}
+//                );
             }
         } catch(JSONException e) {
             e.printStackTrace();
         } finally {
             trips.close();
+            db.close();
+        }
+    }
+
+    private void syncSamples() {
+        Log.d(TAG, "syncSamples");
+
+        FloatingCarDbHelper dbHelper = FloatingCarDbHelper.getInstance(getApplicationContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        Cursor samples = db.query(FloatingCarContract.SampleEntry.TABLE_NAME,
+                new String[]{
+                        FloatingCarContract.SampleEntry.TIMESTAMP,
+                        FloatingCarContract.SampleEntry.SHA_256,
+                        FloatingCarContract.SampleEntry.SHA_TRIP,
+                        FloatingCarContract.SampleEntry.SYNCED
+                },
+                FloatingCarContract.SampleEntry.SYNCED + " = ?",
+                new String[]{"0"}, null, null, null
+        );
+
+        try {
+            final int timestampIndex = samples.getColumnIndex(FloatingCarContract.SampleEntry.TIMESTAMP);
+            final int shaIndex = samples.getColumnIndex(FloatingCarContract.SampleEntry.SHA_256);
+            final int shaTripIndex = samples.getColumnIndex(FloatingCarContract.SampleEntry.SHA_TRIP);
+            final int syncedIndex = samples.getColumnIndex(FloatingCarContract.SampleEntry.SYNCED);
+
+            while (samples.moveToNext()) {
+                JSONObject sampleJSON = new JSONObject();
+                sampleJSON.put(FloatingCarContract.SampleEntry.TIMESTAMP, samples.getString(timestampIndex));
+                sampleJSON.put(FloatingCarContract.SampleEntry.SHA_256, samples.getString(shaIndex));
+                sampleJSON.put(FloatingCarContract.SampleEntry.SHA_TRIP, samples.getString(shaTripIndex));
+
+                sendJSONObject(sampleJSON, "samples");
+
+                /* if syncing with server is successful then update SYNCED column */
+            }
+        } catch (JSONException e) {
+        } finally {
+            samples.close();
+            db.close();
         }
     }
 
